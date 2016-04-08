0dd5ce1891e1b478f3763c97e14d739f9e7c0255
Chibisov Gennady <web-chib@yandex-team.ru>
2016-04-09 00:05:31 +0300

    added move files and create directory steps

--- cli_bdd/core/steps/file.py
+++ cli_bdd/core/steps/file.py
@@ -16,7 +16,7 @@ class CopyFileOrDirectory(StepBase):
     """
     Examples:
         Given I copy a file from "/tmp/old.txt" to "/var/new.txt"
-        Given I copy the file name "hello.txt" to "/var/"
+        Given I copy the file named "hello.txt" to "/var/"
         Given I copy a directory from "/tmp/hello/" to "/var/"
     """
     type_ = 'given'
@@ -32,6 +32,42 @@ class CopyFileOrDirectory(StepBase):
             shutil.copytree(source, destination)
 
 
+class MoveFileOrDirectory(StepBase):
+    """
+    Examples:
+        Given I move a file from "/tmp/old.txt" to "/var/new.txt"
+        Given I move the file named "hello.txt" to "/var/"
+        Given I move a directory from "/tmp/hello/" to "/var/"
+    """
+    type_ = 'given'
+    sentence = (
+        'I move (a|the) (?P<file_or_directory>(file|directory))'
+        '( (named|from))? "(?P<source>[^"]*)" to "(?P<destination>[^"]*)"'
+    )
+
+    def step(self, file_or_directory, source, destination):
+        shutil.move(source, destination)
+
+
+class CreateDirectory(StepBase):
+    """
+    Examples:
+        Given a directory "/tmp/test/"
+        Given the directory named "/tmp/test/"
+    """
+    type_ = 'given'
+    sentence = (
+        '(a|the) directory'
+        '( named)? "(?P<dir_path>[^"]*)"'
+    )
+
+    def step(self, dir_path):
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+
 base_steps = {
     'copy_file_or_directory': CopyFileOrDirectory,
+    'move_file_or_directory': MoveFileOrDirectory,
+    'create_directory': CreateDirectory,
 }
--- tests/unit/steps/file/tests.py
+++ tests/unit/steps/file/tests.py
@@ -6,7 +6,10 @@ from mock import Mock
 from hamcrest import (
     assert_that,
     equal_to,
-    has_entries
+    has_entries,
+    calling,
+    raises,
+    is_not,
 )
 
 from testutils import (
@@ -48,14 +51,15 @@ class FileStepsMixin(object):
 
     def test_copy_file_or_directory__directory(self):
         original_dir_path = os.path.join(tempfile.gettempdir(), 'original_dir/')
-        original_subdir_path = os.path.join(
-            original_dir_path, 'original_dir/subdir/'
-        )
+        original_subdir_path = os.path.join(original_dir_path, 'subdir/')
         original_subdir_path_file = os.path.join(
             original_subdir_path, 'file.txt'
         )
-        original_subdir_path_file_path_content = 'some content'
+        original_subdir_path_file_content = 'some content'
         new_dir_path = os.path.join(tempfile.gettempdir(), 'new_dir/')
+        new_subdir_path_file = os.path.join(
+            new_dir_path, 'subdir/file.txt'
+        )
 
         try:
             shutil.rmtree(original_dir_path)
@@ -67,7 +71,7 @@ class FileStepsMixin(object):
 
         # add content to original file
         with open(original_subdir_path_file, 'wr') as ff:
-            ff.write(original_subdir_path_file_path_content)
+            ff.write(original_subdir_path_file_content)
 
         try:
             shutil.rmtree(new_dir_path)
@@ -84,9 +88,111 @@ class FileStepsMixin(object):
         )
         assert_that(os.path.exists(original_subdir_path_file), equal_to(True))
         assert_that(
-            open(original_subdir_path_file).read(),
-            equal_to(original_subdir_path_file_path_content)
+            open(new_subdir_path_file).read(),
+            equal_to(original_subdir_path_file_content)
         )
+
+    def test_move_file_or_directory__file(self):
+        original_file_path = os.path.join(tempfile.gettempdir(), 'file.txt')
+        original_file_text = 'some text'
+        new_file_path = os.path.join(tempfile.gettempdir(), 'new_file.txt')
+
+        # add content to original file
+        with open(original_file_path, 'wr') as ff:
+            ff.write(original_file_text)
+
+        try:
+            os.remove(new_file_path)
+        except OSError:
+            pass
+
+        self.execute_module_step(
+            'move_file_or_directory',
+            kwargs={
+                'file_or_directory': 'file',
+                'source': original_file_path,
+                'destination': new_file_path
+            }
+        )
+        assert_that(os.path.exists(original_file_path), equal_to(False))
+        assert_that(os.path.exists(new_file_path), equal_to(True))
+        assert_that(open(new_file_path).read(), equal_to(original_file_text))
+
+    def test_move_file_or_directory__directory(self):
+        original_dir_path = os.path.join(tempfile.gettempdir(), 'original_dir/')
+        original_subdir_path = os.path.join(original_dir_path, 'subdir/')
+        original_subdir_path_file = os.path.join(
+            original_subdir_path, 'file.txt'
+        )
+        original_subdir_path_file_content = 'some content'
+        new_dir_path = os.path.join(tempfile.gettempdir(), 'new_dir/')
+        new_subdir_path_file = os.path.join(
+            new_dir_path, 'subdir/file.txt'
+        )
+
+        try:
+            shutil.rmtree(original_dir_path)
+        except OSError:
+            pass
+
+        # create origin dir
+        os.makedirs(original_subdir_path)
+
+        # add content to original file
+        with open(original_subdir_path_file, 'wr') as ff:
+            ff.write(original_subdir_path_file_content)
+
+        try:
+            shutil.rmtree(new_dir_path)
+        except OSError:
+            pass
+
+        self.execute_module_step(
+            'move_file_or_directory',
+            kwargs={
+                'file_or_directory': 'directory',
+                'source': original_dir_path,
+                'destination': new_dir_path
+            }
+        )
+        assert_that(os.path.exists(original_dir_path), equal_to(False))
+        assert_that(os.path.exists(original_subdir_path_file), equal_to(False))
+        assert_that(
+            open(new_subdir_path_file).read(),
+            equal_to(original_subdir_path_file_content)
+        )
+
+    def test_create_directory(self):
+        dir_path = os.path.join(tempfile.gettempdir(), 'some_dir/')
+
+        try:
+            shutil.rmtree(dir_path)
+        except OSError:
+            pass
+
+        self.execute_module_step(
+            'create_directory',
+            kwargs={
+                'dir_path': dir_path
+            }
+        )
+        assert_that(os.path.exists(dir_path), equal_to(True))
+        assert_that(os.path.isdir(dir_path), equal_to(True))
+
+        # test if directory already exists
+        assert_that(
+            calling(self.execute_module_step).with_args(
+                'create_directory',
+                kwargs={
+                    'dir_path': dir_path
+                }
+            ),
+            is_not(
+                raises(OSError)
+            )
+        )
+        assert_that(os.path.exists(dir_path), equal_to(True))
+        assert_that(os.path.isdir(dir_path), equal_to(True))
 
 
 class TestFileStepsSentenceRegex(StepsSentenceRegexTestMixin, TestCase):
@@ -103,6 +209,56 @@ class TestFileStepsSentenceRegex(StepsSentenceRegexTestMixin, TestCase):
                     }
                 }
             }
+        ],
+        'move_file_or_directory': [
+            {
+                'value': 'I move a file "one.txt" to "two.txt"',
+                'expected': {
+                    'kwargs': {
+                        'file_or_directory': 'file',
+                        'source': 'one.txt',
+                        'destination': 'two.txt'
+                    }
+                }
+            },
+            {
+                'value': 'I move a file from "one.txt" to "two.txt"',
+                'expected': {
+                    'kwargs': {
+                        'file_or_directory': 'file',
+                        'source': 'one.txt',
+                        'destination': 'two.txt'
+                    }
+                }
+            },
+            {
+                'value': 'I move a file named "one.txt" to "two.txt"',
+                'expected': {
+                    'kwargs': {
+                        'file_or_directory': 'file',
+                        'source': 'one.txt',
+                        'destination': 'two.txt'
+                    }
+                }
+            },
+        ],
+        'create_directory': [
+            {
+                'value': 'a directory "/tmp/test/"',
+                'expected': {
+                    'kwargs': {
+                        'dir_path': '/tmp/test/',
+                    }
+                }
+            },
+            {
+                'value': 'the directory named "/tmp/test/"',
+                'expected': {
+                    'kwargs': {
+                        'dir_path': '/tmp/test/',
+                    }
+                }
+            },
         ],
     }
 
